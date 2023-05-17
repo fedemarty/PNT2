554696b6161e9389fa9238e4cf6966104ce8beff
GONZALO ZUBELDIA <38675500@comunidadort.edu.ar>
2023-05-16 23:02:16 -0300

    Actualizacion clase 7

---
+++ src/services/listaService.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "https://6464027e127ad0b8f895db50.mockapi.io/",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+const lista = "/lista/"
+
+export default {
+  async cargar() {
+    try {
+      const response = await apiClient.get(lista);
+      return response.data;
+    } catch (error) {
+      throw "Error de conexion";
+    }
+  },
+  async agregar(elem) {
+    try {
+      await apiClient.post(lista, elem);
+    } catch (error) {
+      throw "Error de conexion";
+    }
+  },
+  async eliminar(id) {
+    try {
+      await apiClient.delete(lista + id);
+    } catch (error) {
+      throw "Error de conexion";
+    }
+  },
+  async modificar(id, elem) {
+    try {
+      await apiClient.put(lista + id, elem);
+    } catch (error) {
+      throw "Error de conexion";
+    }
+  },
+};
--- src/views/SystemView.vue
+++ src/views/SystemView.vue
@@ -4,13 +4,16 @@
     <h2>System</h2>
     <ion-content>
       <ion-list v-for="e in lista" :key="e.id">
-        {{ e.id }} {{ e.desc }} 
+        {{ e.id }} {{ e.Descripcion }} 
+      <ion-button @click="eliminar(e.id)">Eliminar</ion-button>
+      <ion-button @click="modificar(e.id)">Modificar</ion-button>
       </ion-list>  
       <ion-input 
         label="id" label-placement="stacked" v-model="elemento.id"></ion-input>
       <ion-input 
-        label="Descripcion" label-placement="stacked" v-model="elemento.desc"></ion-input>
+        label="Descripcion" label-placement="stacked" v-model="elemento.Descripcion"></ion-input>
       <ion-button @click="agregar"> Agregar a la lista </ion-button>
+      <ion-button @click="cargarLista">Cargar Lista</ion-button>
     </ion-content>  
     <ion-button @click="irahome">Ir a Home</ion-button>
   </ion-page>
@@ -18,16 +21,61 @@
 
 <script>
 import {IonPage, IonButton, IonContent, IonInput, IonList} from '@ionic/vue'
+//import axios from 'axios'
+import listaService from '../services/listaService'
 
 export default {
   components: { IonPage, IonButton, IonContent, IonInput, IonList},
   data() {
     return {
-      lista: [{id:100,desc:'Silla'},{id:200,desc:'mesa'}],
-      elemento: {}
+      lista:[],
+      elemento:{},
+      elemento: { id: "", desc: "" }
     }
   },
- 
+  methods: {
+    irahome() {
+      this.$router.push("/");
+    },
+    async agregar() {
+      try {
+        const elem = { ...this.elemento }
+        //await axios.post("https://6464027e127ad0b8f895db50.mockapi.io/lista",elem)
+        await listaService.agregar(elem)
+        this.cargarLista()
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async cargarLista(){
+      try {
+        //const response = await axios.get("https://6464027e127ad0b8f895db50.mockapi.io/lista")
+        //this.lista = response.data
+        this.lista = await listaService.cargar()
+      } catch(e){
+        alert(e)
+      }
+    },
+    async eliminar(id) {
+      try {
+        //await axios.delete("https://6464027e127ad0b8f895db50.mockapi.io/lista/" + id)
+        await listaService.eliminar(id)
+        await this.cargarLista()
+      } catch ( error) {
+          alert(error)
+      }
+    },
+    async modificar(id) {
+      try {
+        const elem = { ...this.elemento }
+        //await axios.put("https://6464027e127ad0b8f895db50.mockapi.io/lista/" + id, elem)
+        await listaService.modificar(id,elem)
+        await this.cargarLista()
+      } catch ( error) {
+          alert(error)
+      }
+    },
+  },
 }
 </script>
 
